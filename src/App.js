f6af0ddd842fc9e02758d6bbf0c9b63491255d34
SolomonPipi <solomonpipi1@gmail.com>
2024-04-09 13:09:08 +0100

    added card for recommendations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import { useState } from "react";
 import PromptBox from "./Components/PromptBox/PromptBox";
+import RecommendationsPage from "./Pages/RecommendationsPage/RecommendationsPage";
 
 function App() {
   const apiKey = process.env.REACT_APP_API_KEY;
   const [content, setContent] = useState(null);
-  const [content2, setContent2] = useState(null);
-  const [content3, setContent3] = useState(null);
-  const [originalPrompt, setOriginalPrompt] = useState(null);
 
   const handleSendRequest = async (selectedOption) => {
     try {
       const response = await processMessageToChatGPT(selectedOption);
       setContent(response.choices[0]?.message?.content);
-
-      const response2 = await processMessageToChatGPT(selectedOption);
-      setContent2(response2.choices[0]?.message?.content);
-
-      const response3 = await processMessageToChatGPT(selectedOption);
-      setContent3(response3.choices[0]?.message?.content);
 
       if (content) {
         const chatGPTResponse = {
@@ -54,34 +46,12 @@ function App() {
     return responseData.json();
   }
 
-  // const handleNewResponse = async (event) => {
-  //   event.preventDefault();
-
-  //   const secondPrompt = `I dont like ${content}.${originalPrompt}  `;
-
-  //   try {
-  //     const newResponse = await processMessageToChatGPT([secondPrompt]);
-  //     setContent(newResponse.choices[0]?.message?.content);
-  //     console.log(newResponse);
-  //     if (content) {
-  //       const chatGPTResponse = {
-  //         message: content,
-  //         sender: "ChatGPT",
-  //       };
-  //     }
-  //   } catch (error) {
-  //     console.error("Error processing message:", error);
-  //   }
-  // };
-
   return (
     <>
       <div className="chat">
         <PromptBox onSend={handleSendRequest} />
         <div className="content">
-          {content && <p className="content__box">{content}</p>}
-          {content2 && <p className="content__box">{content2}</p>}
-          {content3 && <p className="content__box">{content3}</p>}
+          <RecommendationsPage recommendedAnime={content} />
         </div>
       </div>
     </>
--- src/Pages/RecommendationsPage/RecommendationsPage.jsx
+++ src/Pages/RecommendationsPage/RecommendationsPage.jsx
@@ -1,5 +1,44 @@
-import React from "react";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import axios from "axios";
 
-export default function RecommendationsPage() {
-  return;
+export default function RecommendationsPage({ recommendedAnime }) {
+  const [renderRecommendedAnime, setRenderRecommendedAnime] =
+    React.useState(null);
+
+  const MALapiURL = "https://api.jikan.moe/v4/anime";
+
+  const handleRecommendedAnime = async () => {
+    try {
+      const response = await axios.get();
+      setRenderRecommendedAnime();
+
+      return;
+    } catch (error) {
+      console.error("Error rendering anime:", error);
+    }
+  };
+
+  return (
+    <>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia sx={{ height: 140 }} image="" title="" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div"></Typography>
+          <Typography variant="body2" color="text.secondary">
+            {recommendedAnime}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </>
+  );
 }
